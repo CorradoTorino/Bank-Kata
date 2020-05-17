8d0e4128ae8304fc56a781b65265a333a442faf6
Corrado Calzoni <corrado.calzoni@roche.com>
2020-05-17 23:40:07 +0200

    start testting of PrintStatement

---
+++ Bank-Kata/BankKata.Tests/StatementPrinterTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace BankKata.Tests
+{
+    [TestClass]
+    public class StatementPrinterTests
+    {
+        readonly Mock<IConsole> consoleMock = new Mock<IConsole>();
+        StatementPrinter sut;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.sut = new StatementPrinter(consoleMock.Object);
+        }
+
+        [TestMethod]
+        public void StatementPrinter_WhenTransactionIsEmpty_ReturnsHeader()
+        {
+            // Arrange
+            var expectedOutput = "Date       || Amount || Balance".Replace(" ", "");
+
+            // Act
+            sut.PrintStatement(new List<Transaction>());
+
+            // Assert
+            consoleMock.Verify(console => console.WriteLine(
+                    It.Is<string>(s => s.Replace(" ", "") == expectedOutput)),
+                Times.Once);
+        }
+
+        [TestMethod]
+        public void StatementPrinter_WhenTransactionIsNull_ReturnsHeader()
+        {
+            // Arrange
+            var expectedOutput = "Date       || Amount || Balance".Replace(" ", "");
+
+            // Act
+            sut.PrintStatement(null);
+
+            // Assert
+            consoleMock.Verify(console => console.WriteLine(
+                    It.Is<string>(s => s.Replace(" ", "") == expectedOutput)),
+                Times.Once);
+        }
+
+        [TestMethod]
+        public void StatementPrinter_WhenTransactionsToBePrintedAreNotEmpty_ReturnsHeaderAndTransactions()
+        {
+            // Arrange
+            var consoleOutput = new List<string>();
+            consoleMock.Setup(m => m.WriteLine(Capture.In(consoleOutput)));
+            
+            var transactions = new List<Transaction>()
+            {
+                new Transaction(new DateTime(2010,5,4), 2000)
+            };
+            
+            var expectedOutput = new List<string>()
+            {
+                "Date || Amount || Balance".Replace(" ", ""),
+                "04/05/2010||2000||2000".Replace(" ", "")
+            };
+            
+            // Act
+            sut.PrintStatement(transactions);
+
+            // Assert
+            consoleOutput.Should().BeEquivalentTo(expectedOutput);
+        }
+    }
+}
--- Bank-Kata/BankKata/StatementPrinter.cs
+++ Bank-Kata/BankKata/StatementPrinter.cs
@@ -13,6 +13,26 @@ namespace BankKata
 
         public void PrintStatement(IEnumerable<Transaction> transactions)
         {
+            this.PrintHeader();
+            this.PrintTransactions(transactions);
+        }
+
+        private void PrintHeader()
+        {
+            this.console.WriteLine("Date||Amount||Balance");
+        }
+
+        private void PrintTransactions(IEnumerable<Transaction> transactions)
+        {
+            if (transactions == null)
+            {
+                return;
+            }
+            
+            foreach (var transaction in transactions)
+            {
+                this.console.WriteLine($"{transaction.TransactionDate:dd/MM/yyyy}||{transaction.Amount}||{transaction.Amount}");
+            }
         }
     }
 }
